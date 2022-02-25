85679c277fa8ec1ea2bc35a26a54983fa594ba63
prida082 <beatbrave882@gmail.com>
2022-02-26 00:58:31 +0700

    new01

---
+++ Assets/assetsme02/animator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animator : MonoBehaviour
+{
+    public void getAnimatorDrunk(){
+        this.GetComponent<Animator>().Play("Drunk Walk");
+    }
+    public void getAnimatorStan(){
+        this.GetComponent<Animator>().Play("Standing Walk Right");
+    }
+}
---
+++ Assets/changanimetor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changanimetor : MonoBehaviour
+{
+    public void getAnimatorDefeate(){
+        this.GetComponent<Animator>().Play("Defeated");
+    }
+    public void getAnimatorRumba(){
+        this.GetComponent<Animator>().Play("Rumba Dancing");
+    }
+}
+
